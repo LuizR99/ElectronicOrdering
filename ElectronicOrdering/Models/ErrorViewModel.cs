584bbef7f36dd95b6d230bf6b622688d923efc9f
LuizR99 <luizor49@gmail.com>
2025-08-11 15:11:32 -0300

    Initial commit - project ElectronicOrdering

---
+++ ElectronicOrdering/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ElectronicOrdering.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
